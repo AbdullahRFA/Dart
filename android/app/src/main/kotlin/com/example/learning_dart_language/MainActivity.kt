1310a430eff846694b21ac6193a7588fb1a8d724
AbdullahRFA <shakibrybmn@gmail.com>
2025-08-14 00:20:55 +0600

    added first hellow world program using dart

---
+++ android/app/src/main/kotlin/com/example/learning_dart_language/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learning_dart_language
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Hello_world.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main(){
+  print("Hello World");
+
+  stdout.write("Enter Your Name : ");
+
+  var  name = stdin.readLineSync();
+  print("Welcome $name");
+}
